13e9a3e0464155edcd9f03cc432f713390704246
Mdude <em8d@yandex.ru>
2019-10-02 21:50:23 +0300

    DB migration

---
+++ Migrations/20191002184722_InitialCreate.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebParserTestApp2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GooglePlayTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    IconLink = table.Column<string>(nullable: true),
+                    Rating = table.Column<string>(nullable: true),
+                    SearchQuery = table.Column<string>(nullable: true),
+                    ScreenshotsSum = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GooglePlayTable", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppStoreTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Link = table.Column<string>(nullable: true),
+                    IconLink = table.Column<string>(nullable: true),
+                    Rating = table.Column<string>(nullable: true),
+                    SearchQuery = table.Column<string>(nullable: true),
+                    ScreenshotsSum = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppStoreTable", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MainTable",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SearchQuery = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MainTable", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppStoreTable");
+            migrationBuilder.DropTable(
+                name: "GooglePlayTable");
+            migrationBuilder.DropTable(
+                name: "MainTable");
+        }
+    }
+}
---
+++ Model/AppInfoContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebParserTestApp2.Model
+{
+    public class AppInfoContext : DbContext
+    {
+        public DbSet<MainTable> MainTable { get; set; }
+        public DbSet<AppInfoDb> GoogleApps { get; set; }
+        public DbSet<AppInfoDb> AppStoreApps { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=AppsInfo.db");
+    }
+
+    // В sqlite хранится другая модель, т.к. List<string> не поддерживается
+    public class AppInfoDb
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string IconLink { get; set; }
+        public string Rating { get; set; }
+        public string SearchQuery { get; set; }
+        public string ScreenshotsSum { get; set; }
+    }
+
+    public class MainTable
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SearchQuery { get; set; }
+
+    }
+
+}
