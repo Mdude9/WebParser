b194c8c148b32520062c79ddcbd340c4044a82a5
Mdude <em8d@yandex.ru>
2019-10-02 22:08:44 +0300

    db manager added

---
+++ DbManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebParserTestApp2.Model;
+
+namespace WebParserTestApp2
+{
+    class DbManager
+    {
+        //public async bool Func(string searchStr)
+        //{
+        //    using (var context = new AppInfoContext())
+        //    {
+        //        var temp = context.MainTable
+        //            .Find()
+        //    }
+        //}
+    }
+}
